bbb910e71d05bae27e5d2936f3bdcad2d49b7d74
midoit01 <midoit01@hs-esslingen.de>
2022-03-30 09:50:25 -0400

    ContactUs page

--- home/src/views/ContactUs.vue
+++ home/src/views/ContactUs.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-        <h1 class="ma-4 text-center text-h1">Contact Us</h1>
+        <h1 class="pa-4 text-center text-h1">Contact Us</h1>
 
-        <v-container class="ma-4 text-center text-h4">
+        <v-container class="pa-2 text-center text-h4">
             <p>If you would like to receive help regarding Home Utilities Monitor, feel free to reach out to us. <br><br><br>
                 You can find us at:
                 <br>
