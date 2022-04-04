c1e8f6b572b8e940ae29ef39e160fc8f7b817bb9
Callum Kaspick <callumkaspick@gmail.com>
2022-04-03 22:43:26 -0400

    Get usages working on empty db

--- server/src/controllers/GetUsagesController.js
+++ server/src/controllers/GetUsagesController.js
@@ -38,17 +38,21 @@ module.exports = {
             console.log("inserted all mock data")
             const mockElectricSeconds = await MockElectricSeconds.findAll({
                 where: {
-                    ElectricDeviceElectricDeviceID: electricDevice.dataValues.electricDeviceID
-                }
+                    ElectricDeviceElectricDeviceID: electricDevice.dataValues.electricDeviceID,
+                    date: [1,2,3,4,5,6,7,8,9,10,10, 11, 12, 13, 
+                        14, 15, 16, 17, 18, 19,
+                        20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+                        30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+                        40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
+                        50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
+                        60]
+                },
+                raw: true
             })
-            
-            console.log(mockElectricSeconds.toJSON())
-                
             console.log("found all mock entries matching device ID")
-            const mockElectricSecondsJSON = mockElectricSeconds.toJSON()
-            console.log(mockElectricSecondsJSON)
+            console.log(mockElectricSeconds)
             res.send({
-                mockElectricSeconds: mockElectricSecondsJSON
+                mockElectricSeconds: mockElectricSeconds
             })
 
         } catch (err) {
