6639a208538942897bb439545c839afdb9b19b49
Callum Kaspick <callumkaspick@gmail.com>
2022-04-13 17:32:38 -0400

    Michaels changes

--- home/src/views/AboutView.vue
+++ home/src/views/AboutView.vue
@@ -1,9 +1,20 @@
 <template>
     <div>
-        <h1 class="ma-4">About Us</h1>
-
-        <v-container class="my-4">
-            content
+        <h1 class="ma-4 text-center text-h2">About Us</h1>
+        <v-divider></v-divider>
+        <v-container class="text-center my-4 text-h5">
+            <p>Home Utilities Monitor is a device that is integrated into a home's utilities installations 
+                (water, electric) that is paired with a mobile application that will monitor 
+                all usage of the utilities and provide feedback directly to a user's mobile device. This is
+                to ensure maximum transparency for users and equip them with the insight into their habits 
+                and ability to be more knowledgeable about how they use the everyday tools around them. Instead
+                 of receiving a bill and taking your provider's word for your usage, Home Utilities Monitor 
+                 gives you specific data and can even keep track of things like your vampire electricity i.e. 
+                 everything that's plugged in even while it's not being used. Our goal with this project is to 
+                 not just simulate an innovation on a small scale, but deliver a fully working product at a low 
+                 cost that could be brought to a bigger market one day. Even if that doesn’t happen then we want 
+                 to gain valuable project experience that can enhance our portfolios and prepare us for professional 
+                 life outside of college.</p>
         </v-container>
     </div>
 </template>
