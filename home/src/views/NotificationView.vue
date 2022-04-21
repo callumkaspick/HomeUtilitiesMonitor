019f3a4dbfa2fb7b9a14dd9d7cd1a638d0583988
midoit01 <midoit01@hs-esslingen.de>
2022-04-21 01:28:03 -0400

    add mock notifications

--- home/src/views/NotificationView.vue
+++ home/src/views/NotificationView.vue
@@ -1,6 +1,47 @@
 <template>
-    <div id="app" class="ui-container">
-        <h1 class="ma-4 pa-4 text-center text-h2">Notifications</h1>
+    <div>
+        <h1 class="ma-4 text-center text-h1">Notifications</h1>
         <v-divider></v-divider>
+        
+        <div class="text-center">
+            <v-btn class="text-center mt-4" dark color="appSecondary" @click="show = true"> Mock Notifications </v-btn>
+            <v-snackbar text :style="{'margin-bottom':calcMargin(i)}" v-for="(s,i) in snackbars" :key="i" v-model="show" :timeout="false" :min-width="1000">
+                {{ s.text }}
+                <v-btn rounded color="appPrimary" @click="hide(i)" >
+                    <v-icon>close</v-icon>
+                </v-btn>
+            </v-snackbar>
+        </div>
     </div>
 </template>
+
+<script>
+import NotificationVue from "@/components/Notification.vue";
+
+export default {
+    components: {
+        NotificationVue
+    },
+    data() {
+        return {
+            snackbars: [
+            {text: "You have successfully added a water device. "},
+            {text: "You have successfully added an electric device. "},
+            {text: "You have used more water than your expected limit for this billing period. "},
+            {text: "You have used more electricity than your expected limit for this billing period. "},
+            {text: "Warning! A water leak has been detected in your household. "},
+            {text: "Warning! You are using a high amount of vampire energy. This will affect your costs for this billing period. "}        
+            ],
+            show: false
+            }
+    },
+    methods: {
+        calcMargin(i) {
+            return (i*100) + 'px'
+        },
+        hide(i){
+            this.snackbars.splice(i,1)
+        }
+    }
+}
+</script>
